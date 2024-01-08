5cfbe193ade11e29d050f1d5ed3d765776894fb9
r7belle <rborral27@gmail.com>
2024-01-08 18:17:15 +0800

    Added flexbox

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
-<!-- App.vue -->
-<script setup>
-import BaseCondition from './BaseCondition.vue';
-import BaseLoop from './BaseLoop.vue';
-</script>
-
 <template>
-  <div>
+  <div id="app">
     <BaseCondition/>
-    <BaseLoop/>
+    <BaseLoop />
+    <FlexBox :showComponents="true"/>
   </div>
 </template>
+
+<script>
+import BaseCondition from './Components/BaseCondition.vue';
+import BaseLoop from './Components/BaseLoop.vue';
+import FlexBox from './Components/FlexBox.vue';
+export default {
+  components: {
+    BaseCondition,
+    BaseLoop,
+    FlexBox,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Helvetica Neue', sans-serif;
+}
+</style>
